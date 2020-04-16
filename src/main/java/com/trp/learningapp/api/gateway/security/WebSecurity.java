6ce0c2890433947d5de94fd4fdd00e9e94ee824a
Tusar Patel <tusarrpatel@hotmail.com>
2020-04-16 16:42:37 +0200

    gateway initial commit

---
+++ src/main/java/com/trp/learningapp/api/gateway/security/WebSecurity.java
@@ -0,0 +1,38 @@
+package com.trp.learningapp.api.gateway.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter{
+	
+	Environment env;
+	
+	@Autowired
+	public WebSecurity(Environment env) {
+		this.env = env;
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.POST,env.getProperty("api.login.urlpath")).permitAll()
+		.antMatchers(HttpMethod.POST,env.getProperty("api.registration.urlpath")).permitAll()
+		.anyRequest().authenticated();
+		
+		//Never create session
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		
+	}
+
+}
---
+++ src/test/java/com/trp/learningapp/api/gateway/LearningAppApiZuulGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trp.learningapp.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningAppApiZuulGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
